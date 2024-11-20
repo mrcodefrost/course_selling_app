e12f8af9bb20cabb25f7d88d94621a35b26f0a49
mrcodefrost <mr.codefrost@gmail.com>
2024-11-20 17:41:12 +0530

    MongoDB connected with initial schema

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { userRouter } = require('./routes/user');
 const { courseRouter } = require('./routes/course');
 const { adminRouter } = require('./routes/admin');
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 
 app.use("/user", userRouter);
@@ -9,4 +11,13 @@ app.use("/admin", adminRouter);
 app.use("/course", courseRouter);
 
 
-app.listen(3000);
+async function main() {
+    console.log("Connecting to mongodb");
+    await mongoose.connect(process.env.MONGODB_URL);
+    app.listen(3000);
+    console.log("Connected to mongodb");
+    console.log("Listening on port 3000");
+}
+
+
+main();
